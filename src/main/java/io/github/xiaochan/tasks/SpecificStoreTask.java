9d923e516f74416f24a831a0b21072b388fb02ed
lyrric <xx20510@126.com>
2026-04-17 18:03:09 +0800

    refactor(tasks): 提取BaseTask基类统一各任务逻辑
    - 新增BaseTask基类，封装执行流程、消息发送和历史记录保存
    - MinimumPayService继承BaseTask，重写业务过滤逻辑
    - SpecificStoreTask继承BaseTask，重写业务过滤逻辑和成功后状态更新
    - 移除MinimumPayService和SpecificStoreTask中的重复执行和消息发送代码
    - 新增SpringContextUtil用于Spring上下文获取辅助

--- src/main/java/io/github/xiaochan/tasks/SpecificStoreTask.java
+++ src/main/java/io/github/xiaochan/tasks/SpecificStoreTask.java
@@ -15,11 +15,11 @@ import io.github.xiaochan.model.enums.NotifyTypeEnums;
 import io.github.xiaochan.service.*;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -31,7 +31,7 @@ import java.util.stream.Collectors;
  */
 @Component
 @Slf4j
-public class SpecificStoreTask {
+public class SpecificStoreTask extends BaseTask {
 
 
     @Resource
@@ -59,7 +59,7 @@ public class SpecificStoreTask {
             List<NotifyConfigEntity> notifyConfigList = notifyConfigService.list(NotifyTypeEnums.STORE_ACTIVITY, NotifyConfigStatusEnums.ENABLE);
             log.info("开始执行 指定门店活动定时任务 获取到{}个配置信息", notifyConfigList.size());
             for (NotifyConfigEntity notifyConfig : notifyConfigList) {
-                specifyStoreActivityRemind(notifyConfig);
+                runSingle(notifyConfig);
             }
         }catch (Exception e){
             log.error("执行指定门店活动定时任务时发生异常", e);
@@ -76,82 +76,27 @@ public class SpecificStoreTask {
     /**
      * 指定门店活动提醒
      */
-    private void specifyStoreActivityRemind(NotifyConfigEntity notifyConfig){
-        TaskExecHistoryEntity execHistory = new TaskExecHistoryEntity();
-        execHistory.setUserId(notifyConfig.getUserId());
-        execHistory.setNotifyConfigId(notifyConfig.getId());
-        execHistory.setStartTime(LocalDateTime.now());
+    @Override
+    protected List<StoreInfo> doRunSingle(NotifyConfigEntity notifyConfig, TaskExecHistoryEntity execHistory, LocationEntity location) {
         execHistory.setNotifyType(NotifyTypeEnums.STORE_ACTIVITY);
-        execHistory.setSuccess(true);
-        try {
-            log.info("开始执行指定门店活动提醒 {}", notifyConfig.getId());
-            //获取地址
-            Optional<LocationEntity> optionalLocation = locationService.getOptById(notifyConfig.getLocationId());
-            if (optionalLocation.isEmpty()) {
-                log.error("位置信息不存在 {} {}", notifyConfig.getId(), notifyConfig.getLocationId());
-                notifyConfigService.updateConfig(notifyConfig.getId(), NotifyConfigStatusEnums.DISABLE, "位置信息不存在");
-                execHistory.setSuccess(false);
-                execHistory.setRemark("执行失败：位置信息不存在");
-                return;
-            }
-            LocationEntity location = optionalLocation.get();
-            StoreExtNotifyConfig storeExtNotifyConfig = JSON.parseObject(notifyConfig.getExtConfig(), StoreExtNotifyConfig.class);
-            //通过搜索来获取门店活动信息
-            List<StoreInfo> storeInfos = xiaoChanService.searchList(storeExtNotifyConfig.getStoreInfo().getName(),
-                    location.getCityCode(), location.getLongitude(),
-                    location.getLatitude());
-            List<StoreInfo> availableStores = storeInfos
-                    .stream()
-                    //同一个门店
-                    .filter(storeInfo -> storeExtNotifyConfig.getStoreInfo().getStoreId().equals(storeInfo.getStoreId()))
-                    .filter(storeInfo -> storeInfo.getLeftNumber() > 0)
-                    //返现金额必须大于等于之前的返现金额
-                    .filter(storeInfo -> storeInfo.getRebatePrice().compareTo(storeExtNotifyConfig.getStoreInfo().getRebatePrice()) >= 0)
-                    //价格必须小于等于之前的价格
-                    .filter(storeInfo -> storeInfo.getPrice().compareTo(storeExtNotifyConfig.getStoreInfo().getPrice()) <= 0)
-                    .toList();
-            execHistory.setNotifyStoreCount(availableStores.size());
-            if(availableStores.isEmpty()){
-                log.info("configId: {} 没有满足条件的门店活动", notifyConfig.getId());
-                return;
-            }
-            log.info("configId: {} 找到{}个满足条件的门店活动", notifyConfig.getId(), availableStores.size());
-            notifyConfigService.updateConfig(notifyConfig.getId(), NotifyConfigStatusEnums.DISABLE, "任务完成，完成时间" + DateUtil.format(new Date(), "MM-dd HH:mm:ss"));
-            //通知
-            sendMessage(availableStores, location);
-        }catch (Exception e){
-            log.error("执行指定门店活动提醒时发生异常 {}", notifyConfig.getId(), e);
-            execHistory.setSuccess(false);
-            execHistory.setRemark("执行失败:" + e.getMessage());
-        }finally {
-            execHistory.setEndTime(LocalDateTime.now());
-            taskExecHistoryService.save(execHistory);
-        }
-
+        StoreExtNotifyConfig storeExtNotifyConfig = JSON.parseObject(notifyConfig.getExtConfig(), StoreExtNotifyConfig.class);
+        //通过搜索来获取门店活动信息
+        List<StoreInfo> storeInfos = xiaoChanService.searchList(storeExtNotifyConfig.getStoreInfo().getName(),
+                location.getCityCode(), location.getLongitude(),
+                location.getLatitude());
+        return storeInfos
+                .stream()
+                //同一个门店
+                .filter(storeInfo -> storeExtNotifyConfig.getStoreInfo().getStoreId().equals(storeInfo.getStoreId()))
+                .filter(storeInfo -> storeInfo.getLeftNumber() > 0)
+                //返现金额必须大于等于之前的返现金额
+                .filter(storeInfo -> storeInfo.getRebatePrice().compareTo(storeExtNotifyConfig.getStoreInfo().getRebatePrice()) >= 0)
+                //价格必须小于等于之前的价格
+                .filter(storeInfo -> storeInfo.getPrice().compareTo(storeExtNotifyConfig.getStoreInfo().getPrice()) <= 0)
+                .toList();
     }
-
-
-
-    private void sendMessage(List<StoreInfo> storeInfos, LocationEntity locationEntity) {
-        String body = storeInfos.stream()
-                .map(storeInfo -> buildMessage(storeInfo, locationEntity))
-                .collect(Collectors.joining("<br/><br/>"));
-        UserEntity userEntity = userService.getById(locationEntity.getUserId());
-        try {
-            MessageHttp.sendMessage(userEntity.getSpt(), body, "有新的返现活动啦");
-        }catch (Exception e){
-            log.error("发送消息失败", e);
-        }
-    }
-
-    private String buildMessage(StoreInfo storeInfo, LocationEntity locationEntity) {
-        return "地点：" + locationEntity.getName() + "<br/>" +
-                "平台：" + StorePlatformEnum.getByType(storeInfo.getType()).name + "<br/>" +
-                "店铺：" + storeInfo.getName() + "<br/>" +
-                "时间范围：" + storeInfo.getStartTime() + "-" + storeInfo.getEndTime() + "<br/>" +
-                "距离：" + storeInfo.getDistance() + "米" + "<br/>" +
-                "库存：" + storeInfo.getLeftNumber() + "<br/>" +
-                "规则：满" + storeInfo.getPrice() + "返" + storeInfo.getRebatePrice() + "<br/>" +
-                "是否需要评价:" + (storeInfo.getRebateCondition() == null ? "未知" : (storeInfo.getRebateCondition() != 99 ? "是" : "否")) + "\r\n";
+    @Override
+    protected void afterSuccess(NotifyConfigEntity notifyConfig) {
+        notifyConfigService.updateConfig(notifyConfig.getId(), NotifyConfigStatusEnums.DISABLE, "任务完成，完成时间" + DateUtil.format(new Date(), "MM-dd HH:mm:ss"));
     }
 }
